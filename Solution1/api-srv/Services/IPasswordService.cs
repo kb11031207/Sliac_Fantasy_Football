0811f2bba4e0185445edbf21c5fb1c8f606541e5
Kesiena Berezi <berezikesiena@gmail.com>
2025-10-05 16:41:57 -0500

    commit

--- Solution1/api-srv/Services/IPasswordService.cs
+++
@@ -1,8 +0,0 @@
-namespace api_srv.Services
-{
-    public interface IPasswordService
-    {
-        (byte[] hash, byte[] salt) HashPassword(string password);
-        bool VerifyPassword(string password, byte[] hash, byte[] salt);
-    }
-}
